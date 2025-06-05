993fa1697fad3f5447de049be9be01df20168e49
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2025-06-05 22:33:13 +0530

    Add files via upload

---
+++ components/contact_info.php
@@ -0,0 +1,28 @@
+<div class="address rounded-corner">
+    <div class="title">
+        <h1>Our Contact</h1>
+    </div>
+    <div class="box-container">
+        <div class="box">
+            <i class="bx bxs-map-alt"></i>
+            <div>
+                <h4>Address</h4>
+                <p>2436, General Thimayya Road,<br> East St, Camp,<br>Pune, Maharashtra 411001</p>
+            </div>
+        </div>
+        <div class="box">
+            <i class="bx bxs-phone-incoming"></i>
+            <div>
+                <h4>Phone Number</h4>
+                <p><a href="tel:+91 7712345677">+91 7712345677</a></p>
+            </div>
+        </div>
+        <div class="box">
+            <i class="bx bxs-envelope"></i>
+            <div>
+                <h4>Email</h4>
+                <p><a href="mailto:info@foodflow.com">info@foodflow.com</a></p>
+            </div>
+        </div>
+    </div>
+</div> 
